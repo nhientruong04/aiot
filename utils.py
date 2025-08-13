c9fde2ef9375a927566056f1b734c938dc127cd0
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-08-13 15:17:00 +0100

    feat: finish specs calculation

--- utils.py
+++ utils.py
@@ -3,16 +3,13 @@ from multiprocessing import Queue
 from tqdm import tqdm
 from typing import List
 from common.infer_model import HailoInfer
-from typing import Optional
+from typing import Optional, Tuple
 
 # WARNING: This is a very very unoptimized piece
-def teacher_normalization(teacher_hef_path: str, dataset_path: str,
-                          save_path: Optional[str] = None):
+def teacher_normalization(teacher: HailoInfer, dataset_path: str):
     dataset = np.load(dataset_path)
     assert dataset.dtype == np.uint8, f'Expected uint8, got {dataset.dtype}'
     items: List = np.split(dataset, dataset.shape[0], axis=0)
-
-    teacher = HailoInfer('../data/teacher.hef', input_type='UINT8', output_type='FLOAT32')
 
     # teacher_mean
     output_means = list()
@@ -37,21 +34,63 @@ def teacher_normalization(teacher_hef_path: str, dataset_path: str,
     channel_std = np.mean(np.stack(output_stds, axis=0), axis=0)
     channel_std = channel_std[None, None, :]
 
-    teacher.close()
-
-    if save_path is not None:
-        _, ext = save_path.split('.')
-        file_path = None
-        if ext == 'npz':
-            file_path = save_path
-        else:
-            file_path = f'{save_path}.npz'
-
-        print(f'Saving to {file_path}.')
-        np.savez(file_path, channel_mean=channel_mean, channel_std=channel_std)
-
     return channel_mean, channel_std
 
+def map_normalization(teacher: HailoInfer, student: HailoInfer, channel_mean: np.float32,
+                      channel_std: np.float32,  dataset_path: str):
+    dataset = np.load(dataset_path)
+    assert dataset.dtype == np.uint8, f'Expected uint8, got {dataset.dtype}'
+    items: List = np.split(dataset, dataset.shape[0], axis=0)
+
+    maps = list()
+
+    for item in tqdm(items, desc='Calculating map normalization indices.'):
+        teacher_output = teacher.run_sync(item)
+        teacher_output = (teacher_output - channel_mean) / channel_std
+        student_output = student.run_sync(item)
+        map = np.mean((teacher_output - student_output)**2, axis=2, keepdims=True)
+        maps.append(map)
+
+    maps_st = np.concatenate(maps)
+    q_st_start = np.quantile(maps_st, 0.9)
+    q_st_end = np.quantile(maps_st, 0.995)
+
+    return q_st_start, q_st_end
+
+# channel_norm_file should be the training dataset and map_norm_file should be the validation dataset
+def prepare_specs(teacher_hef: str, student_hef: str, channel_norm_file: str,
+                  map_norm_file: str, save_file: Optional[str] = None) -> dict[str, np.array]:
+    teacher = HailoInfer(teacher_hef, input_type='UINT8', output_type='FLOAT32')
+    student = HailoInfer(student_hef, input_type='UINT8', output_type='FLOAT32')
+
+    channel_mean, channel_std = teacher_normalization(teacher, '../data/v3_train_dataset.npy')
+    q_st_start, q_st_end = map_normalization(teacher, student, channel_mean, channel_std, '../data/v3_train_dataset.npy')
+
+    if save_file is not None:
+        _, ext = save_file.split('.')
+        file_path = None
+        if ext == 'npz':
+            file_path = save_file
+        else:
+            file_path = f'{save_file}.npz'
+
+        print(f'Saving to {file_path}.')
+        np.savez(file_path, channel_mean=channel_mean, channel_std=channel_std,
+                 q_st_start=q_st_start, q_st_end=q_st_end)
+
+    teacher.close()
+    student.close()
+
+    return {
+        'channel_mean': channel_mean,
+        'channel_std': channel_std,
+        'q_st_start': q_st_start,
+        'q_st_end': q_st_end
+    }
+
+
 if __name__ == "__main__":
-    channel_mean, channel_std = teacher_normalization('../data/teacher.hef', '../data/v3_train_dataset.npy',
-                                                      'teacher_norm.npz')
+    specs = prepare_specs('../data/teacher.hef', '../data/student.hef', 
+                  '../data/v3_train_dataset.npy', '../data/v3_train_dataset.npy', 'specs.npz')
+
+    print(specs['q_st_start'], specs['q_st_end'])
