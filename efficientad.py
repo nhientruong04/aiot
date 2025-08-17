957c58080bf3cca410a6dd34ec79f7b4dd667beb
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-08-17 09:55:17 +0100

    draft for full testing pipeline

---
+++ efficientad.py
@@ -0,0 +1,177 @@
+import numpy as np
+from multiprocessing import Queue
+from tqdm import tqdm
+from typing import List
+from common.infer_model import HailoInfer
+from typing import Optional, Tuple
+from dataset import Dataset, DataLoader
+from PIL import Image
+from sklearn.metrics import roc_curve, roc_auc_score
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def test(test_loader: DataLoader, teacher: HailoInfer, student: HailoInfer, autoencoder: HailoInfer,
+         teacher_mean: np.float32, teacher_std: np.float32, q_st_start: Optional[np.array] = None,
+         q_st_end: Optional[np.array] = None, q_ae_start: Optional[np.array] = None,
+         q_ae_end: Optional[np.array] = None, test_output_dir: Optional[str] = None,
+         plot_path: str = 'output.png', desc='Running inference'):
+    assert test_loader.batch_size == 1
+
+    y_true = []
+    y_score = []
+
+    for images, targets, paths in tqdm(test_loader, desc=desc):
+        image, target, path = images[0], targets[0], paths[0]
+        with Image.open(path) as img:
+            orig_width, orig_height = img.width, img.height
+
+        map_combined, map_st, map_ae = predict(
+            image=image, teacher=teacher, student=student,
+            autoencoder=autoencoder, teacher_mean=teacher_mean,
+            teacher_std=teacher_std, q_st_start=q_st_start, q_st_end=q_st_end,
+            q_ae_start=q_ae_start, q_ae_end=q_ae_end)
+
+        if test_output_dir is not None:
+            img_nm = os.path.split(path)[1].split('.')[0]
+            if not os.path.exists(os.path.join(test_output_dir, target)):
+                os.makedirs(os.path.join(test_output_dir, target))
+            file = os.path.join(
+                test_output_dir, target, img_nm + '.tiff')
+            tifffile.imwrite(file, map_combined)
+
+        y_true_image = 0 if target == 'good' else 1
+        y_score_image = np.max(map_combined)
+        y_true.append(y_true_image)
+        y_score.append(y_score_image)
+
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    auc = roc_auc_score(y_true=y_true, y_score=y_score)
+
+    plt.figure(figsize=(6,6))
+    plt.plot(fpr, tpr, color='blue', lw=2, label=f'ROC curve (AUC = {auc:.3f})')
+    plt.plot([0,1], [0,1], color='gray', linestyle='--', label='Random guess')
+    plt.xlabel('False Positive Rate (FPR)')
+    plt.ylabel('True Positive Rate (TPR)')
+    plt.title('Receiver Operating Characteristic (ROC)')
+    plt.legend(loc="lower right")
+    plt.grid(alpha=0.3)
+    plt.savefig(plot_path)
+
+    metrics = {
+        'AUC': auc
+    }
+    df = pd.DataFrame(metrics.items(), columns=["Metric", "Value"])
+    print(df.to_string(index=False))
+
+    return auc, fpr, tpr, thresholds
+
+
+def map_normalization(teacher: HailoInfer, student: HailoInfer, autoencoder: HailoInfer,
+                      teacher_mean: np.float32, teacher_std: np.float32, loader: DataLoader):
+    """
+    dataset.DataLoader loader: dataloader for train or val split. The original code uses val split.
+    """
+    assert loader.batch_size == 1
+
+    maps_st = list()
+    maps_ae = list()
+
+    for images, _, _ in tqdm(loader, desc='Calculating map normalization indices'):
+        map_combined, map_st, map_ae = predict(images[0], teacher, student, autoencoder,
+                                               teacher_mean, teacher_std)
+        maps_st.append(map_st)
+        maps_ae.append(map_ae)
+
+    maps_st = np.concatenate(maps_st)
+    maps_ae = np.concatenate(maps_ae)
+
+    q_st_start = np.quantile(maps_st, 0.9)
+    q_st_end = np.quantile(maps_st, 0.995)
+    q_ae_start = np.quantile(maps_ae, 0.9)
+    q_ae_end = np.quantile(maps_ae, 0.995)
+
+    return q_st_start, q_st_end, q_ae_start, q_ae_end
+
+
+def teacher_normalization(teacher: HailoInfer, train_loader: DataLoader):
+    assert train_loader.batch_size == 1
+
+    # teacher_mean
+    output_means = list()
+
+    for images, _, _ in tqdm(train_loader, desc='Calculating teacher\'s channels mean'):
+        output = teacher.run_sync(images[0])
+        output_mean = np.mean(output, axis=(0,1))
+        output_means.append(output_mean)
+
+    channel_mean = np.mean(np.stack(output_means, axis=0), axis=0)
+    channel_mean = channel_mean[None, None, :]
+
+    # teacher_std
+    output_stds = list()
+
+    for images, _, _ in tqdm(train_loader, desc='Calculating teacher\'s channels std'):
+        output = teacher.run_sync(images[0])
+        distance = (output - channel_mean) ** 2
+        distance_mean = np.mean(distance, axis=(0,1))
+        output_stds.append(distance_mean)
+
+    channel_std = np.mean(np.stack(output_stds, axis=0), axis=0)
+    channel_std = channel_std[None, None, :]
+
+    return channel_mean, channel_std
+
+
+def predict(image: np.array, teacher: HailoInfer, student: HailoInfer,
+            autoencoder: HailoInfer, teacher_mean: np.array, teacher_std: np.array,
+            q_st_start: Optional[np.array] = None, q_st_end: Optional[np.array] = None,
+            q_ae_start: Optional[np.array] = None, q_ae_end: Optional[np.array] = None):
+
+    teacher_output = teacher.run_sync(image)
+    teacher_output = (teacher_output - teacher_mean) / teacher_std
+    student_output = student.run_sync(image)
+    autoencoder_output = autoencoder.run_sync(image)
+
+    # WARNING: this could be a potential error if fc layer change number of output channels
+    map_st = np.mean((teacher_output - student_output[:, :, :384])**2, axis=2, keepdims=True)
+    map_ae = np.mean((teacher_output - student_output[:, :, 384:])**2, axis=2, keepdims=True)
+
+    if q_st_start is not None:
+        map_st = 0.1 * (map_st - q_st_start) / (q_st_end - q_st_start)
+    if q_ae_start is not None:
+        map_ae = 0.1 * (map_ae - q_ae_start) / (q_ae_end - q_ae_start)
+    map_combined = 0.5 * map_st + 0.5 * map_ae
+
+    return map_combined, map_st, map_ae
+
+if __name__ == "__main__":
+    teacher_hef = '../models/orig_bottle/teacher.hef'
+    student_hef = '../models/orig_bottle/student.hef'
+    autoencoder_hef = '../models/orig_bottle/autoencoder.hef'
+    test_dir = '/home/nhien/aiot/data/bottle/test'
+    train_dir = '/home/nhien/aiot/data/bottle/train'
+    size = (256, 256)
+
+    teacher = HailoInfer(teacher_hef, input_type='UINT8', output_type='FLOAT32')
+    student = HailoInfer(student_hef, input_type='UINT8', output_type='FLOAT32')
+    autoencoder = HailoInfer(autoencoder_hef, input_type='UINT8', output_type='FLOAT32')
+
+    test_set = Dataset(test_dir)
+    test_loader = DataLoader(test_set, shuffle=True, resize=size)
+
+    train_set = Dataset(train_dir)
+    train_loader = DataLoader(train_set, shuffle=True, resize=size)
+
+    # teacher_mean, teacher_std = teacher_normalization(teacher, train_loader)
+    # q_st_start, q_st_end, q_ae_start, q_ae_end = map_normalization(teacher, student, autoencoder,
+    #                                                                teacher_mean, teacher_std, train_loader)
+    specs_file = np.load('../models/orig_bottle/specs.npz')
+    specs = {key: specs_file[key] for key in specs_file.files}
+
+    auc = test(test_loader, teacher, student, autoencoder, **specs)
+    print(auc)
+
+    teacher.close()
+    student.close()
+    autoencoder.close()
--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 import numpy as np
+import os
 from multiprocessing import Queue
 from tqdm import tqdm
 from typing import List
 from common.infer_model import HailoInfer
 from typing import Optional, Tuple
+from datetime import datetime
+from sklearn.metrics import precision_recall_curve, average_precision_score, roc_curve, roc_auc_score
+from matplotlib import pyplot as plt
 
 # WARNING: This is a very very unoptimized piece
 def teacher_normalization(teacher: HailoInfer, dataset_path: str):
@@ -36,8 +40,8 @@ def teacher_normalization(teacher: HailoInfer, dataset_path: str):
 
     return channel_mean, channel_std
 
-def map_normalization(teacher: HailoInfer, student: HailoInfer, channel_mean: np.float32,
-                      channel_std: np.float32,  dataset_path: str):
+def map_normalization_noae(teacher: HailoInfer, student: HailoInfer, channel_mean: np.float32,
+                      channel_std: np.float32, dataset_path: str):
     dataset = np.load(dataset_path)
     assert dataset.dtype == np.uint8, f'Expected uint8, got {dataset.dtype}'
     items: List = np.split(dataset, dataset.shape[0], axis=0)
@@ -89,8 +93,75 @@ def prepare_specs(teacher_hef: str, student_hef: str, channel_norm_file: str,
     }
 
 
-if __name__ == "__main__":
-    specs = prepare_specs('../data/teacher.hef', '../data/student.hef', 
-                  '../data/v3_train_dataset.npy', '../data/v3_train_dataset.npy', 'specs.npz')
+class AD_Evaluation():
+    def __init__(self, y_true: np.array, y_score: np.array, plot_dir: str = datetime.now().strftime('%Y-%m-%d-%H')):
+        self.y_true = y_true
+        self.y_score = y_score
+        self.plot_dir = os.path.join('.', 'runs', plot_dir)
+        self.__post_init__()
 
-    print(specs['q_st_start'], specs['q_st_end'])
+    def __post_init__(self):
+        self.__aggregate()
+        os.makedirs(self.plot_dir, exist_ok=True)
+
+    def __aggregate(self):
+        # Calculate ROC
+        self.fpr, self.tpr, thresholds = roc_curve(y_true=self.y_true, y_score=self.y_score)
+        # Calculate AUC
+        self.auc = roc_auc_score(y_true=self.y_true, y_score=self.y_score)
+
+        # Find best threshold
+        J = self.tpr - self.fpr
+        self.ix = J.argmax()
+        self.J_threshold = thresholds[self.ix]
+
+        # Find Precision and Recall
+        self.precision, self.recall, self.pr_thresholds = precision_recall_curve(y_true=self.y_true, y_score=self.y_score)
+        self.ap = average_precision_score(y_true=self.y_true, y_score=self.y_score)
+
+    def visualize_pr_curve(self) -> None:
+        fig, ax = plt.subplots(figsize=(6, 6))
+        ax.plot(self.recall, self.precision, lw=2, label=f'PR curve (AP = {self.ap:.3f})')
+        ax.set_xlabel('Recall')
+        ax.set_ylabel('Precision')
+        ax.set_title('Precision-Recall Curve')
+        ax.legend(loc="lower left")
+        ax.grid(alpha=0.3)
+
+        return fig
+
+    def visualize_roc(self) -> None:
+        fig, ax = plt.subplots(figsize=(6, 6))
+
+        # Plot ROC curve
+        ax.plot(self.fpr, self.tpr, color='blue', lw=2, label=f'ROC curve (AUC = {self.auc:.3f})')
+        ax.plot([0,1], [0,1], color='gray', linestyle='--', label='Random guess')
+
+        # Mark optimal threshold
+        ax.scatter(self.fpr[self.ix], self.tpr[self.ix], color='red', label=f'Best thr={self.J_threshold:.2f}')
+        ax.set_xlabel('False Positive Rate (FPR)')
+        ax.set_ylabel('True Positive Rate (TPR)')
+        ax.set_title('Receiver Operating Characteristic (ROC)')
+        ax.legend(loc="lower right")
+        ax.grid(alpha=0.3)
+
+        return fig
+
+    def evaluate(self, show: bool = False):
+        roc_fig = self.visualize_roc()
+        pr_fig = self.visualize_pr_curve()
+
+        roc_fig.savefig(os.path.join(self.plot_dir, 'roc_curve.png'))
+        pr_fig.savefig(os.path.join(self.plot_dir, 'precision_recall_curve.png'))
+
+        if show:
+            roc_fig.show()
+            pr_fig.show()
+
+        plt.close(roc_fig)
+        plt.close(pr_fig)
+
+
+if __name__ == "__main__":
+    evaluation_model = AD_Evaluation(np.random.randint(0, 2, 10), np.random.random(10))
+    evaluation_model.evaluate()
